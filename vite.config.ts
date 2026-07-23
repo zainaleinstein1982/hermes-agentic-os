1b6af2db4e5658a9d45aed9aa5e20ce0b7a6315a
zainaleinstein1982 <88931669+zainaleinstein1982@users.noreply.github.com>
2026-07-23 14:35:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hermes-agentic-os/', // <--- Tambahkan baris ini
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
