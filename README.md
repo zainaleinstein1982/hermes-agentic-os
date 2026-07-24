a42fb5727e7e3fabbc8230a7f7d92ae46bb18739
zainaleinstein1982 <88931669+zainaleinstein1982@users.noreply.github.com>
2026-07-24 10:39:23 +0700

    Updated vite.config.ts

--- README.md
+++
@@ -1,3 +0,0 @@
-# hermes-agentic-os
-
-[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-1ms5sfrr)
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hermes-agentic-os/', // <--- Tambahkan baris ini
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
